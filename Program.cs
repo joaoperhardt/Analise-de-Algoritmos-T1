c6b74089fd8b3f1b68521df90acfb88bf69977d5
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2026-03-09 21:02:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Analise_de_Algoritmos_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
