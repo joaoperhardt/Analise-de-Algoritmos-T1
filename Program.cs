aa2b29d6a847c7177abdedf0a7be4cfea2051d03
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2026-03-16 19:05:01 -0300

    excluindo classe cliente

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ namespace Analise_de_Algoritmos_T1
     {
         static void Main(string[] args)
         {
-            new Cliente().Executar();
-            Console.WriteLine("\nPressione qualquer tecla para sair...");
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
