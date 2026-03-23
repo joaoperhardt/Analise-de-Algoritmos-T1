1f5d5382c14af05feb087884af02a64a79c5a459
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2026-03-23 19:13:33 -0300

    trabalho parte 2

---
+++ Enum/TipoOrdem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analise_de_Algoritmos_T1.Enum
+{
+    public enum TipoOrdem
+    {
+        Compra,
+        Venda
+    }
+}
---
+++ Model/Ordem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analise_de_Algoritmos_T1.Enum;
+
+namespace Analise_de_Algoritmos_T1.Model
+{
+    public class Ordem
+    {
+        public TipoOrdem tipoOrdem { get; private set; }
+        public Investidor investidor { get; private set; }
+        public double valor { get; private set; }
+        public Ordem(TipoOrdem tipoOrdem, Investidor investidor, double valor)
+        {
+            this.tipoOrdem = tipoOrdem;
+            this.investidor = investidor;
+            this.valor = valor;
+        }
+    }
+}
